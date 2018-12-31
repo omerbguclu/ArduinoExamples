0eacb620a87937b400f13bf0f894887c1cc29397
Omer Buyukguclu <Omer Buyukguclu>
2019-01-01 01:07:56 +0300

    Update tutorials-01-interrupts.ino

--- tutorials-01-interrupts/tutorials-01-interrupts.ino
+++ tutorials-01-interrupts/tutorials-01-interrupts.ino
@@ -1,6 +1,8 @@
 int ledPin =         13;
+int potpin =          0;
 boolean led_state = LOW;
 int i =               0;
+int val;
 
 void setup(){
   pinMode(ledPin,OUTPUT);
@@ -12,7 +14,7 @@ void setup(){
   TCNT1  = 0;
   TCCR1A = 0;
   TCCR1B = 0;
-  OCR1A = 15624;  // 1 saniye çalışması için gerekli zaman kesmesi frekansı
+  OCR1A = 15624/5;  // 1/5 saniye çalışması için gerekli zaman kesmesi frekansı
   TCCR1B |= (1 << WGM12);
   TCCR1B |= (1 << CS12) | (1 << CS10);
   TIMSK1 |= (1 << OCIE1A);
@@ -21,12 +23,17 @@ void setup(){
 
 
 ISR(TIMER1_COMPA_vect){     //Her kesmeye girildiğinde otomatik çalıştırılacak fonksiyon.
-  led_state = !led_state;
-  digitalWrite(ledPin, led_state);
+  val = analogRead(potpin);            // reads the value of the potentiometer (value between 0 and 1023)
+  Serial.print("Servo Position: ");
+  Serial.println(val);
 }
 
 void loop(){
-  Serial.print("loop ");
-  Serial.println(i++);
+  Serial.print("Loop count: ");
+  Serial.print(i++);
+  Serial.print(" Led state change to -> ");
+  led_state = !led_state;
+  digitalWrite(ledPin, led_state);
+  Serial.println(led_state);
   delay(5000);
 }
